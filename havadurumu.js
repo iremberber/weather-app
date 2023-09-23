0ce2367704b383961a024dc77748764eaed7e48a
iremberber <iremberber0@gmail.com>
2023-09-23 15:21:30 +0300

    .citycard şehir ekleme

--- havadurumu.js
+++ havadurumu.js
@@ -9,6 +9,7 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 const resultsBox = document.querySelector(".result-box");
 const detail = document.querySelector(".detail")
+const card = document.querySelector(".card")
 
 const istanbulDiv = document.getElementById("istanbul");
 const newYorkDiv = document.getElementById("newyork");
@@ -227,7 +228,7 @@ document.querySelector(".weather").style.display = "none";
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
     resultsBox.innerHTML = '';
-    sehirler.style.display = 'none';
+    citycard.style.marginLeft= "-470px";
     
 })
 
@@ -239,12 +240,75 @@ arrowimg.style.cursor = "pointer";
 arrowimg.addEventListener("click", () => {
     isCityCardVisible = !isCityCardVisible;
     
-    // Citycard'ın görünürlüğüne göre margin-right değerini ayarlayın
+   
     if (isCityCardVisible) {
         citycard.style.marginLeft = "0";
     } else {
-        citycard.style.marginLeft = "-420px"; // veya başka bir değer
+        citycard.style.marginLeft = "-420px"; 
     }
+
+    if (isCityCardVisible) {
+        citycard.classList.add("visible");
+    } else {
+        citycard.classList.remove("visible");
+    }
+
+});
+
+async function getCityWeather(cityName, resultBox) {
+    const apiKey = "a11b257e77b811a987bcdf9f0bc82e6a";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+    try {
+        const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
+        if (response.status === 200) {
+            const data = await response.json();
+            const temperature = Math.round(data.main.temp) + "°C";
+            const humidity = "%" + data.main.humidity;
+            const windSpeed = data.wind.speed + "km/sa";
+
+            const weatherHTML = `
+                <h1 class="temp">${temperature}</h1>
+                <h2 class="city">${cityName}</h2>
+                <div class="detail">
+                    <div class="col">
+                        <img src="https://cdn-icons-gif.flaticon.com/11095/11095576.gif" id="drop">
+                        <div>
+                            <p class="humidity">${humidity}</p>
+                            <p>Nem</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <img src="https://cdn-icons-gif.flaticon.com/6455/6455039.gif" id="wind">
+                        <div>
+                            <p class="wind">${windSpeed}</p>
+                            <p>Rüzgar Hızı</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            resultBox.innerHTML = weatherHTML;
+        } else {
+            resultBox.innerHTML = "Hava durumu bilgisi bulunamadı.";
+        }
+    } catch (error) {
+        resultBox.innerHTML = "Hava durumu bilgileri alınırken bir hata oluştu.";
+    }
+}
+
+document.getElementById("input-box2").addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        const cityName = document.getElementById("input-box2").value;
+        const resultBox = document.querySelector(".result-box2");
+        getCityWeather(cityName, resultBox);
+    }
+});
+
+document.querySelector(".search2 button").addEventListener("click", () => {
+    const cityName = document.getElementById("input-box2").value;
+    const resultBox = document.querySelector(".result-box2");
+    getCityWeather(cityName, resultBox);
 });
 
 checkWeather();
